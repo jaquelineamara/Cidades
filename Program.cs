249c2e9e86090e902689bfeeeff0dc96e148ad3d
jaquelineamara <jaquelineamara5@gmail.com>
2022-03-22 20:44:35 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Mongágua");
+Console.WriteLine("Santos");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Praia Grande");
